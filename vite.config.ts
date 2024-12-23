0449d314f8a5788719e80e77161fb3efdf59ade0
Maksym Shmelyov <maks@Maksyms-MacBook-Pro.local>
2024-12-23 13:04:27 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://bumblebee-sh.github.io/test_/',
   plugins: [
     vue(),
     vueDevTools(),
